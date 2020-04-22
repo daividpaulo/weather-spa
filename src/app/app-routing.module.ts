ca3d618eb0cb4538484c81fd8bc8be1beda7542e
DAIVID PAULO VANDERLEI FLORENCIO DE SOUSA <daivid.paulo@unimedcaruaru.com.br>
2020-04-21 21:33:37 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardWeatherComponent } from './dashboard-weather/dashboard-weather.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: DashboardWeatherComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
